1daffdb3c39a24da37ad83d2c1f6fbc5191487f1
IGCBOOM <caya1.carter1@gmail.com>
2024-03-28 12:25:24 -0500

    Fix oregen, add Material chains group.

--- kubejs/startup_scripts/worldgen.js
+++ kubejs/startup_scripts/worldgen.js
@@ -1,3 +1,8 @@
 WorldgenEvents.remove(event =>  {
-    event.removeFeatureById('underground_ores', ['create:zinc_ore', 'forbidden_arcanus:xpetrified_ore', 'forbidden_arcanus:arcane_crystal_ore', 'forbidden_arcanus:stella_arcanum', 'forbidden_arcanus:runic_stone', 'iceandfire:silver_ore', 'iceandfire:deepslate_silver_ore', 'iceandfire:sapphire_ore'])
+    event.removeFeatureById('underground_ores', 
+    ['create:zinc_ore', 'forbidden_arcanus:xpetrified_ore', 
+    'forbidden_arcanus:arcane_crystal_ore', 'forbidden_arcanus:stella_arcanum', 
+    'forbidden_arcanus:runic_stone', 'iceandfire:silver_ore', 
+    'iceandfire:deepslate_silver_ore', 'iceandfire:sapphire_ore', 
+    'create_dd:tin_ore', 'create_dd:deepslate_tin_ore'])
 })
